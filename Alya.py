25f8a941bd93e0b7993e886e11aca0d7aebd323e
女ね子 <151407806+onnaneco@users.noreply.github.com>
2026-05-23 14:24:12 +0300

    Made Alya.py a web service

--- Alya.py
+++ Alya.py
@@ -4,6 +4,21 @@ from psycopg2 import pool
 from groq import Groq
 import os
 import time
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive!"
+
+def run():
+  app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 
 # --- CONFIGURATION ---
 
@@ -334,4 +349,5 @@ if __name__ == "__main__":
     print("Checking database...")
     init_db()
     print("Bot is running on Render!")
+    keep_alive()
     bot.infinity_polling()
